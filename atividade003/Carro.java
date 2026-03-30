429e979472ca54c94eca779a45b9878d8acee2a4
Nicolas <163208984+NicolasHeling@users.noreply.github.com>
2026-03-30 16:16:57 -0300

    atualizacao

--- atividade003/Carro.java
+++ atividade003/Carro.java
@@ -1,19 +1,17 @@
 public class Carro {
-   
     private String marca;
     private String modelo;
-  
+    private int ano; // O erro do "ano" acontecia porque essa linha estava faltando ou escrita diferente!
 
-   
     public Carro(String marca, String modelo, int ano) {
         this.marca = marca;
         this.modelo = modelo;
         this.ano = ano;
     }
 
-
     public void acelerar() {
-        System.out.println("O " + modelo + " está acelerando: Vrummm!");
+        // Usando a marca e o ano aqui para o aviso (triângulo amarelo) sumir
+        System.out.println("O " + marca + " " + modelo + " do ano " + ano + " está acelerando: Vrummm!");
     }
 
     public void frear() {
